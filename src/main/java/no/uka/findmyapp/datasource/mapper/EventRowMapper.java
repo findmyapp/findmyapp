27ea3bab1bc1bbec7372386d69d0550a580e0183
Torstein Melhus Barkve <torstein@barkve.no>
2011-06-29 09:00:43 +0200

    rettet opp feilaktig sletting av EventRowMapper.java

---
+++ src/main/java/no/uka/findmyapp/datasource/mapper/EventRowMapper.java
@@ -0,0 +1,37 @@
+package no.uka.findmyapp.datasource.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import no.uka.findmyapp.model.Event;
+import org.springframework.jdbc.core.RowMapper;
+
+public class EventRowMapper implements RowMapper<Event> {
+
+
+	public Event mapRow(ResultSet rs, int arg1) throws SQLException {
+		Event event = new Event();
+		event.setId(rs.getInt("id"));//returnerer id fra event_showing_real
+		event.setShowingTime(rs.getDate("showing_time"));
+		event.setPublishTime(rs.getDate("publish_time"));
+		event.setPlace(rs.getString("place"));
+		event.setBilligId(rs.getInt("billig_id"));
+		event.setEventId(rs.getInt("event_id"));
+		event.setNetsaleFrom(rs.getDate("netsale_from"));
+		event.setNetsaleTo(rs.getDate("netsale_to"));
+		event.setFree(rs.getBoolean("free"));
+		event.setCanceled(rs.getBoolean("canceled"));
+		event.setEntranceId(rs.getInt("entrance_id"));
+		event.setTitle(rs.getString("title"));
+		event.setLead(rs.getString("lead"));
+		event.setText(rs.getString("text"));
+		event.setEventType(rs.getString("event_type"));
+		event.setImage(rs.getString("image"));
+		event.setThumbnail(rs.getString("thumbnail"));
+		event.setHiddenFromListing(rs.getBoolean("hidden_from_listing"));
+		event.setSlug(rs.getString("slug"));
+		event.setAgeLimit(rs.getInt("age_limit"));
+		event.setDetailPhotoId(rs.getInt("detail_photo_id"));
+		return event;
+	}
+
+}
