e478c1cd1bbfeed1a1731f29cb36662b486bfa51
Jostein Guldal <jostein.guldal@accenture.com>
2011-06-30 02:41:59 +0200

    Added jdbcTemplate to context. DataSource is now hosted mysql. Needs credentials.properties to connect

---
+++ src/main/java/no/uka/findmyapp/service/AppStoreService.java
@@ -0,0 +1,40 @@
+package no.uka.findmyapp.service;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import no.uka.findmyapp.datasource.AppStoreRepository;
+import no.uka.findmyapp.model.AppStore.App;
+import no.uka.findmyapp.model.AppStore.AppStoreList;
+import no.uka.findmyapp.model.AppStore.ListType;
+import no.uka.findmyapp.model.AppStore.Platform;
+
+@Service
+public class AppStoreService {
+	@Autowired
+	private AppStoreRepository data;
+
+	private static final Logger logger = LoggerFactory
+	.getLogger(AppStoreService.class);
+	
+	public AppStoreList getAppStoreListForPlatform(int count, ListType listType, Platform platform) {
+		List<App> appList = data.getAppList(count, listType, platform);
+		
+		AppStoreList appStoreList = new AppStoreList();
+		appStoreList.setListType(listType);
+		appStoreList.setPlatform(platform);
+		appStoreList.setListCount(appList.size());
+		appStoreList.setRequestCount(count);
+		appStoreList.setAppList(appList);
+		
+		return appStoreList;
+	}
+	
+	public App getAppDetails(int appId) {
+		return data.getAppDetails(appId);
+	}
+}
