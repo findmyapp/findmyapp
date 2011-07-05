82e9e3f7f9b8c0f677b3cd4aa32a05dfbc161964
Ole Christian Røed <ocroed@gmail.com>
2011-07-05 14:15:01 +0200

    Lagt til top 10 lister i Android Appstore (most Popular og Latest)

--- src/main/java/no/uka/findmyapp/service/AppStoreService.java
+++ src/main/java/no/uka/findmyapp/service/AppStoreService.java
@@ -2,14 +2,14 @@ package no.uka.findmyapp.service;
 
 import java.util.List;
 
+import no.uka.findmyapp.datasource.AppStoreRepository;
+import no.uka.findmyapp.model.appstore.App;
+import no.uka.findmyapp.model.appstore.AppStoreList;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import no.uka.findmyapp.datasource.AppStoreRepository;
-import no.uka.findmyapp.model.appstore.App;
-import no.uka.findmyapp.model.appstore.AppStoreList;
 
 @Service
 public class AppStoreService {
