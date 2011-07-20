a3033279aeb04cd17a4761d1fe60ae61c656b0b9
Christian Askeland <chr.askeland@gmail.com>
2011-07-20 14:58:45 +0200

    Endret event-objekt slik at det kun returnerer nodvendig data

--- src/main/java/no/uka/findmyapp/datasource/mapper/EventRowMapper.java
+++ src/main/java/no/uka/findmyapp/datasource/mapper/EventRowMapper.java
@@ -15,15 +15,15 @@ public class EventRowMapper implements RowMapper<Event> {
 		Event event = new Event();
 		event.setId(rs.getInt("id"));//returnerer id fra event_showing_real
 		event.setShowingTime(rs.getTimestamp("showing_time"));
-		event.setPublishTime(rs.getTimestamp("publish_time"));
+		//event.setPublishTime(rs.getTimestamp("publish_time"));
 		event.setPlace(rs.getString("place"));
 		event.setBilligId(rs.getInt("billig_id"));
-		event.setEventId(rs.getLong("event_id"));
-		event.setNetsaleFrom(rs.getTimestamp("netsale_from"));
-		event.setNetsaleTo(rs.getTimestamp("netsale_to"));
+		//event.setEventId(rs.getLong("event_id"));
+		//event.setNetsaleFrom(rs.getTimestamp("netsale_from"));
+		//event.setNetsaleTo(rs.getTimestamp("netsale_to"));
 		event.setFree(rs.getBoolean("free"));
 		event.setCanceled(rs.getBoolean("canceled"));
-		event.setEntranceId(rs.getInt("entrance_id"));
+		//event.setEntranceId(rs.getInt("entrance_id"));
 		event.setTitle(rs.getString("title"));
 		event.setLead(rs.getString("lead"));
 		event.setText(rs.getString("text"));
