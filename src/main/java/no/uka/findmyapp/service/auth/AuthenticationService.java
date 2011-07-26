76a32b90c10c79b65d3de5343c4c774b28900ec0
Asmund Tokheim <asmund.tokheim@accenture.com>
2011-07-26 20:24:46 +0200

    prover endring i getConsumerDetails

--- src/main/java/no/uka/findmyapp/service/auth/AuthenticationService.java
+++ src/main/java/no/uka/findmyapp/service/auth/AuthenticationService.java
@@ -150,6 +150,14 @@ public class AuthenticationService {
 		UKAppsConsumerDetails details = null;
 		if (authentication.getPrincipal() instanceof UKAppsConsumerDetails) {
 			details = (UKAppsConsumerDetails) authentication.getPrincipal();
+			//Added by aasmund
+			try {
+				details = this.loadConsumer(details.getConsumerKey());
+			}
+			catch (Exception e) {
+				logger.info("FEIL I NOE AASMUND LAGET "+e.getLocalizedMessage());
+			}
+			//End adding by aasmund
 		}
 		return details;
 	}
