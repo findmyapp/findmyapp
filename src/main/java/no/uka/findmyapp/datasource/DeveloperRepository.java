822de2bc90af88e070c747aafd5e6777404a3a8f
Jostein Guldal <jostein.guldal@accenture.com>
2011-07-27 18:10:59 +0200

    debug

--- src/main/java/no/uka/findmyapp/datasource/DeveloperRepository.java
+++ src/main/java/no/uka/findmyapp/datasource/DeveloperRepository.java
@@ -61,15 +61,17 @@ public class DeveloperRepository {
 	}
 	
 	public int updateApp(int developer_id, App app) {
-		return jdbcTemplate.update("UPDATE APPSTORE_APPLICATION SET " +
-				"name = ?, " +
-				"platform = ?, " +
-				"description = ?, " +
-				"market_identifier = ?, " +
-				"facebook_app_id = ?, " +
-				"thumb_image = ? " +
-				"WHERE appstore_developer_id = ? " +
-				"AND appstore_application_id = ?", app.getName(), app.getPlatform(), app.getDescription(), app.getMarketID(), 
+		String sql = "UPDATE APPSTORE_APPLICATION SET " +
+		"name = ?, " +
+		"platform = ?, " +
+		"description = ?, " +
+		"market_identifier = ?, " +
+		"facebook_app_id = ?, " +
+		"thumb_image = ? " +
+		"WHERE appstore_developer_id = ? " +
+		"AND appstore_application_id = ?";
+		logger.info("running sql : " + sql);
+		return jdbcTemplate.update(sql, app.getName(), app.getPlatform(), app.getDescription(), app.getMarketID(), 
 				app.getFacebookAppID(), app.getThumbImage(), developer_id, app.getId());
 	}
 	
