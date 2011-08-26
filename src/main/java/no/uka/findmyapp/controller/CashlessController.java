1d943899c2dc5a891ebd84f1699f4e292069814c
Asmund Tokheim <asmund.tokheim@accenture.com>
2011-08-26 14:59:19 +0200

    La til mulighet for aa sjekke andres cashlessinfo

--- src/main/java/no/uka/findmyapp/controller/CashlessController.java
+++ src/main/java/no/uka/findmyapp/controller/CashlessController.java
@@ -3,6 +3,7 @@ package no.uka.findmyapp.controller;
 import java.util.Date;
 import java.util.List;
 
+import no.uka.findmyapp.exception.InvalidUserIdOrAccessTokenException;
 import no.uka.findmyapp.exception.UkaYearNotFoundException;
 import no.uka.findmyapp.helpers.ServiceModelMapping;
 import no.uka.findmyapp.model.User;
@@ -18,12 +19,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.oauth2.common.exceptions.InvalidTokenException;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -44,10 +49,12 @@ public class CashlessController {
 	 * 
 	 * @return Cashless card with transaction info
 	 */
-	@RequestMapping(value = "/cashless/{ukaYear}/me", method = RequestMethod.GET)
+	@Secured("ROLE_CONSUMER")
+	@RequestMapping(value = "/cashless/{ukaYear}/{idOrMe}", method = RequestMethod.GET)
 	@ServiceModelMapping(returnType=CashlessCard.class)
 	public ModelAndView getMyCashless (
 			@PathVariable String ukaYear,
+			@PathVariable String idOrMe,
 			@RequestParam(required = true) String token,
 			@RequestParam(required=false) @DateTimeFormat(iso = ISO.DATE) Date date,
 			@RequestParam(required=false) @DateTimeFormat(iso = ISO.DATE) Date from,
@@ -56,27 +63,31 @@ public class CashlessController {
 			@RequestParam(required=false) String orderBy) throws ConsumerException, UkaYearNotFoundException{ 
 		
 		//int userId = auth.verify("b7049efc1ccf2eaf829c5cef9ddd186a8c5f8f50i25t1311839388431");
-		int userId = auth.verify( token );
-		logger.debug("UserId: " + userId);
+		int tokenUserId = auth.verify( token );
+		logger.debug("UserId: " + tokenUserId);
 		
 		CashlessCard card;
-		if (userId != -1) {
-			card = cashlessService.getCardTransactions(ukaYear, userId, date, from, to, location);
+		if (tokenUserId != -1 && idOrMe.equalsIgnoreCase("me")) {
+			card = cashlessService.getCardTransactions(ukaYear, tokenUserId, date, from, to, location);
+		} else if (tokenUserId != -1) {
+			card = cashlessService.getCardTransactions(ukaYear, tokenUserId, Integer.parseInt(idOrMe), date, from, to, location);
 		} else {
 			throw new InvalidTokenException("Token not valid");
 		}
 		
 		card.setBalance(cashlessService.getCardBalance(card.getCardNo()));
-		
+		if (!idOrMe.equalsIgnoreCase("me")) {//don't show card number to others
+			card.setCardNo(-1);
+		}
 		return new ModelAndView("json", "card", card);
 	}
-	
 	/**
 	 * Update the users Cashless card number
 	 * 
 	 * @return
 	 */
-	@RequestMapping(value = "/cashless/me/update", method = RequestMethod.GET)
+	@Secured("ROLE_CONSUMER")
+	@RequestMapping(value = "/cashless/me/update", method = RequestMethod.GET)//should maybe be changed to post @ /cashless/me or users/me/cashless
 	@ServiceModelMapping(returnType=CashlessCard.class)
 	public ModelAndView updateMyCashless(
 			@RequestParam(required = true) long cardNo,
@@ -95,7 +106,7 @@ public class CashlessController {
 		
 		return new ModelAndView("json", "status", success);
 	}
-	
+	@Secured("ROLE_CONSUMER")
 	@RequestMapping(value = "/cashless/friends", method = RequestMethod.GET)
 	public ModelAndView getFriendsWithCashless(
 				@RequestParam(required = true) String token) 
@@ -113,9 +124,34 @@ public class CashlessController {
 			return mav;
 	}
 	
-	
 	@RequestMapping(value = "/cashless/{ukaYear}/locations", method = RequestMethod.GET)
 	public ModelAndView getCashlessLocations(@PathVariable String ukaYear) {
 		return new ModelAndView("json", "status", cashlessService.getCashlessLocations());
 	}
+	
+	
+	
+	
+	@SuppressWarnings("unused")
+	@ResponseStatus(HttpStatus.UNAUTHORIZED)
+	@ExceptionHandler(InvalidUserIdOrAccessTokenException.class)
+	private void handleInvalidUserIdOrAccessTokenException(
+			InvalidUserIdOrAccessTokenException e) {
+		logger.debug(e.getMessage());
+	}
+	
+	@SuppressWarnings("unused")
+	@ResponseStatus(HttpStatus.UNAUTHORIZED)
+	@ExceptionHandler(ConsumerException.class)
+	private void handleConsumerException(ConsumerException e) {
+		logger.debug(e.getMessage());
+	}
+	
+	@SuppressWarnings("unused")
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(NumberFormatException.class)
+	private void handleNumberFormatException(NumberFormatException e) {
+		logger.debug(e.getMessage());
+	}
+
 }
