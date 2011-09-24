26ec91467004fec17bfa0ac145868017dafea7c5
Asmund Tokheim <asmund.tokheim@accenture.com>
2011-09-24 17:11:09 +0200

    Rettet opp i cashless design

---
+++ src/main/java/no/uka/findmyapp/exception/PrivacyException.java
@@ -0,0 +1,9 @@
+package no.uka.findmyapp.exception;
+
+public class PrivacyException extends Exception{
+	private static final long serialVersionUID = -6172727654389484682L;
+	
+	public PrivacyException(String msg) {
+		super(msg);
+	}
+}
